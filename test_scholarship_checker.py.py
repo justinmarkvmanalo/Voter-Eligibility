5a9b18c8a6c184c44ef3c016c5d6b9fd8935c3b1
Justinmarkvmanalo <justinmarkvmanalo07@gmail.com>
2025-05-16 19:28:27 +0800

    adding test_eligible_student

---
+++ test_scholarship_checker.py.py
@@ -0,0 +1,14 @@
+import unittest
+from scholarship_checker import is_eligible_for_scholarship
+
+
+class TestScholarshipEligibility(unittest.TestCase):
+
+    def test_eligible_student(self):
+        self.assertTrue(is_eligible_for_scholarship(3.8, 15000))
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
+    
