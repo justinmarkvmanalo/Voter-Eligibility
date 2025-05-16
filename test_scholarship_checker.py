9fd8ff3fedb28d7cbc2999cffb3b9dffbcf2a95d
Justinmarkvmanalo <justinmarkvmanalo07@gmail.com>
2025-05-16 19:53:37 +0800

    add the test for low gpa

--- test_scholarship_checker.py
+++ test_scholarship_checker.py
@@ -7,6 +7,9 @@ class TestScholarshipEligibility(unittest.TestCase):
     def test_eligible_student(self):
         self.assertTrue(is_eligible_for_scholarship(3.8, 15000))
 
+    def test_low_gpa(self):
+        self.assertFalse(is_eligible_for_scholarship(3.4, 18000))
+
 if __name__ == '__main__':
     unittest.main()
     
