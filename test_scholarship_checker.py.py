c3eb1e2a073048dc8d13d8d6fdf4663fe0777a32
Justinmarkvmanalo <justinmarkvmanalo07@gmail.com>
2025-05-16 19:47:28 +0800

    updating the spacing

--- test_scholarship_checker.py.py
+++ test_scholarship_checker.py.py
@@ -7,8 +7,6 @@ class TestScholarshipEligibility(unittest.TestCase):
     def test_eligible_student(self):
         self.assertTrue(is_eligible_for_scholarship(3.8, 15000))
 
-
 if __name__ == '__main__':
     unittest.main()
-    
     
