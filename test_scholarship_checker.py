4662c5b2e145b3b77c2439de7fa5bab0e6fec7e6
Justinmarkvmanalo <justinmarkvmanalo07@gmail.com>
2025-05-16 20:10:01 +0800

    updating

--- test_scholarship_checker.py
+++ test_scholarship_checker.py
@@ -3,18 +3,16 @@ from scholarship_checker import is_eligible_for_scholarship
 
 
 class TestScholarshipEligibility(unittest.TestCase):
+    
 
     def test_eligible_student(self):
         self.assertTrue(is_eligible_for_scholarship(3.8, 15000))
 
-
     def test_low_gpa(self):
         self.assertFalse(is_eligible_for_scholarship(3.0, 25000))
 
-
     def test_high_income(self):
         self.assertFalse(is_eligible_for_scholarship(3.6, 21000))
-
 
 if __name__ == '__main__':
     unittest.main()
